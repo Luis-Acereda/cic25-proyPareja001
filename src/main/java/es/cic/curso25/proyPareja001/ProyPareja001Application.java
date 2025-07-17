5bfd0ea25c074762f11ced700068d67da32f6872
LuisAcereda <no@nomail.no>
2025-07-17 10:38:24 +0200

    Primer commit

---
+++ src/main/java/es/cic/curso25/proyPareja001/ProyPareja001Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyPareja001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyPareja001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyPareja001Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/proyPareja001/ProyPareja001ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyPareja001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyPareja001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
