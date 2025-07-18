80210272553d7f9c4ac343d85c0af91fbadc6569
LuisAcereda <no@nomail.no>
2025-07-18 09:07:08 +0200

    LibroService acabado

--- src/main/java/es/cic/curso25/proyPareja001/controller/LibroController.java
+++ src/main/java/es/cic/curso25/proyPareja001/controller/LibroController.java
@@ -1,16 +1,31 @@
 package es.cic.curso25.proyPareja001.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.cic.curso25.proyPareja001.model.Libro;
 import es.cic.curso25.proyPareja001.service.LibroService;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("libro")
 public class LibroController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LibroController.class);
 
     @Autowired
     LibroService libroService;
 
+    @PostMapping
+    public long create(Libro libro){
+        if (libro.getId() != null) {
+            throw new RuntimeException("Al crear no me puedes pasar id");
+        }
+        this.libroService = null;
+
+        return libroService.create(libro);
+    }
 }
--- src/main/java/es/cic/curso25/proyPareja001/service/LibroService.java
+++ src/main/java/es/cic/curso25/proyPareja001/service/LibroService.java
@@ -1,8 +1,12 @@
 package es.cic.curso25.proyPareja001.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.cic.curso25.proyPareja001.model.Libro;
 import es.cic.curso25.proyPareja001.repository.LibroRepository;
 
 @Service
@@ -10,5 +14,26 @@ public class LibroService {
 
     @Autowired
     LibroRepository libroRepository;
+
+    public long create(Libro libro) {
+        libroRepository.save(libro);
+        return libro.getId();
+    }
+
+    public Libro getById(long id){
+        Optional<Libro> resultado = libroRepository.findById(id);
+        return resultado.orElse(null);
+    }
+    public List<Libro> getAll(){
+        return libroRepository.findAll();
+    }
+
+    public void update(Libro Libro){
+        libroRepository.save(Libro);
+    }
+
+    public void delete(long id){
+        libroRepository.deleteById(id);
+    }
 
 }
